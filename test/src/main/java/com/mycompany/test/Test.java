682de2ab5f578c9b8f53f080c020be58125320dd
ch1kim0n1 <110909601+ch1kim0n1@users.noreply.github.com>
2023-01-03 20:28:20 -0600

    Add files via upload

---
+++ test/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author skyla
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("HI");
+        new MainWindow().setVisible(true);
+    }
+    
+    public void TestMd(){
+        new MainWindow().setVisible(true);
+    }
+
+}
